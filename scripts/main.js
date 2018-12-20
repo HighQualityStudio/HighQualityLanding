b38994ffd1e827fd8c5469434af6f8aa21e96ad3
Alexander Babin <41280415+brightgreenban@users.noreply.github.com>
2018-12-20 21:37:46 +0200

    v0.2

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,21 @@ $(window).on('load', function () {
     $loader.fadeOut();
     $preloader.delay(500).fadeOut('slow');
 });
+
+/* PORTFOLIO */
+ $('.gallery ul li a').click(function() {
+     var itemID = $(this).attr('href');
+     $('.gallery ul').addClass('item_open');
+     $(itemID).addClass('item_open');
+     return false;
+ });
+ $('.close').click(function() {
+     $('.port, .gallery ul').removeClass('item_open');
+     return false;
+ });
+
+ $(".gallery ul li a").click(function() {
+     $('html, body').animate({
+         scrollTop: parseInt($("#top").offset().top)
+     }, 400);
+ });
